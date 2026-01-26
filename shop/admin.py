f24cc9556b6c74f2f81dd14c7cd404ef69250f1a
shashank12-collab <shashanksingh56074@gamil.com>
2026-01-27 00:52:20 +0530

    making homepage

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class show(admin.ModelAdmin):
+    list_display = ('product_name' , 'pub_date')
+
+admin.site.register(Product , show)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=20)
+    desc = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    
+    
+    def __str__(self):
+        return self.product_name
